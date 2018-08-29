753444acdbc4cbc7decabcf0fc0287004f3882eb
jwWood <jwWood87@gmail.com>
2018-08-28 20:53:19 -0600

    Implemented DriverFactory

--- src/main/java/utils/DriverFactory.java
+++ src/main/java/utils/DriverFactory.java
@@ -1,4 +1,43 @@
 package utils;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
 public class DriverFactory {
+
+    public static WebDriver driver;
+
+    public WebDriver getDriver() {
+        try {
+            ReadConfigFile file = new ReadConfigFile();
+            String browserName = file.getBrowser();
+
+            switch (browserName) {
+
+                case "firefox":
+                    if (driver == null) {
+                        System.setProperty("webdriver.gecko.driver", Constant.GECKO_DRIVER_DIRECTORY);
+                        DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+                        capabilities.setCapability("marionatte", true);
+                        driver = new FirefoxDriver();
+                    }
+                    break;
+
+                case "chrome":
+                    if (driver == null) {
+                        System.setProperty("webdriver.chrome.driver", Constant.CHROME_DRIVER_DIRECTORY);
+                        driver = new ChromeDriver();
+                        driver.manage().window().maximize();
+                    }
+                    break;
+            }
+        } catch (Exception e) {
+            System.out.println("Unable to load browser: " + e.getMessage());
+        } finally {
+            driver.manage().window().maximize();
+        }
+        return driver;
+    }
 }
