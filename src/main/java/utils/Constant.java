f49a6e2a91add5042acd88230b9a059e87dfd55d
jwWood <jwWood87@gmail.com>
2018-08-28 21:13:13 -0600

    Implemented Constants

---
+++ src/main/java/utils/Constant.java
@@ -0,0 +1,10 @@
+package utils;
+
+public class Constant {
+
+    public final static String CONFIG_PROPERTIES_DIRECTORY = "";
+
+    public final static String GECKO_DRIVER_DIRECTORY = System.getProperty("user.dir") + "src\\test\\java\\resources\\other\\geckodriver.exe";
+
+    public final static String CHROME_DRIVER_DIRECTORY = System.getProperty("user.dir") + "src\\test\\java\\resources\\other\\chromedriver.exe";
+}
